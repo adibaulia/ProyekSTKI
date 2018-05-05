b4c1fb67194f98f8cea3b47f9c3b16055d41b88b
Adib Aulia <adibaulia@gmail.com>
2018-05-05 10:30:45 +0700

    Tokenizing isok intial 1

--- app/Http/Controllers/ProyekSTKIController.php
+++ app/Http/Controllers/ProyekSTKIController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 class ProyekSTKIController extends Controller
 {
     public function tokenizing(Request $request){
-      dd($request);
+
+      $dokumen = $request->judul_buku.$request->sinopsis;
+      $tokenizerFactory  = new \Sastrawi\Tokenizer\TokenizerFactory();
+      $tokenizer = $tokenizerFactory->createDefaultTokenizer();
+      $tokens = $tokenizer->tokenize($dokumen);
+
+      return view('tokenize')->with('dokumen', $request)->with('token', $tokens);
     }
 }
--- resources/views/tokenize.blade.php
+++ resources/views/tokenize.blade.php
@@ -1,57 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}"  media="screen,projection"/>
-      <title>Proyek STKI</title>
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="{{ asset('css/materialize.min.css') }}" media="screen,projection" />
+  <title>Proyek STKI</title>
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
 
 <body>
-    <div class="section">
+  <div class="section">
 
-      <form class="" action="/tokenizing" method="post">
       <!--   Icon Section   -->
-          <div class="row">
-            <div class="col s12 center">
-              <div class="icon-block">
-                <h5>Tokenize</h5>
-              </div>
-            </div>
+      <div class="row">
+        <div class="col s12 center">
+          <div class="icon-block">
+            <h5>Tokenize</h5>
           </div>
-          <div class="row">
-            <div class="col s6 offset-s3">
-              <table class="striped responsive-table highlight">
-                <thead>
-                  <tr>
-                      <th>Kode Dokumen</th>
-                      <th>Kata/Token</th>
-                  </tr>
-                </thead>
+        </div>
+      </div>
 
-                <tbody>
-                  @for ($i=0; $i < 10; $i++)
-                    <tr>
-                      <td>Alvin</td>
-                      <td>Eclair</td>
-                    </tr>
-                  @endfor
+      <div class="row">
+        <div class="col s6" style="text-align: justify; text-justify: inter-word;">
+          <label for="judul_buku">Judul Buku</label>
+          <p>{{$dokumen->judul_buku}}</p>
+            <label for="sinopsis">Sinopsis Buku</label>
+            <p>{{$dokumen->sinopsis}}</p>
+        </div>
 
+<div class="col s6">
+        <div class="col s3">
+          <table class="striped responsive-table highlight">
+            <thead>
+              <tr>
+                <th style="width: 10%">Kode Dokumen</th>
+                <th>Kata/Token</th>
+              </tr>
+            </thead>
 
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </form>
-    </div>
-
+            <tbody>
+              @foreach ($token as $tokens)
+              <tr>
+                <td>1</td>
+                <td>{{$tokens}}</td>
+              </tr>
+            @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+  </div>
+  </div>
 
 
   <!--  Scripts-->
   <script type="text/javascript" src="{{ asset('js/materialize.min.js') }}"></script>
 
-  </body>
+</body>
+
 </html>
